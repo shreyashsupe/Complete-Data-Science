19b67125b6f58a1659cc224e3150d15f93fc776a
Shreyash Vikas Supe <108145454+shreyashsupe@users.noreply.github.com>
2025-09-23 03:15:46 +0000

    question solved

--- Complete-Python-Bootcamp/0-Python-questions/1.py
+++ Complete-Python-Bootcamp/0-Python-questions/1.py
@@ -3,7 +3,19 @@ Question:
 Create a class Employee with attributes emp_id, name, salary. Write a method to display employee details. Create an object and display its details.
 '''
 
-  
+class Employee:
+    def __init__(self, emp_id, name, salary):
+        self.emp_id = emp_id
+        self.name = name
+        self.salary = salary
 
+    # Method to display employee details
+    def show_details(self):
+        print(f"emp-id:{self.emp_id} Name: {self.name} Salary: {self.salary}")
+    
 
+# Create an object 
+Obj1 = Employee(101, "Amol", 70000)
 
+# Display employee details
+Obj1.show_details()
